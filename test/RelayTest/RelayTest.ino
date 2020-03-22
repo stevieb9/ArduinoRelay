fac292e997d75932596ca725da1524efb709ab50
Steve Bertrand <steveb@cpan.org>
2020-03-22 10:57:40 -0700

    Began modifying initial test sketch

--- test/RelayTest/RelayTest.ino
+++ test/RelayTest/RelayTest.ino
@@ -2,28 +2,47 @@
 #include "TemperatureRelay.h"
 //#include "HumidityRelay.h"
 
-#define RELAY_PIN   2
+#define PIN   2
 
 #define TEMP        70
 #define HUM         60
 #define CYC_ON      1000
 #define CYC_OFF     1000
 
-/* Normal constructor */
-TemperatureRelay ctr(RELAY_PIN, TEMP);
+TemperatureRelay temp(PIN, TEMP);
+HumidityRelay hum(PIN, HUM);
+CycleTimerRelay cyc(PIN, CYC_ON, CYC_OFF);
 
-/* No pin constructor */
-//RelayTest ctr(TEMP);
+
 
 void setup() {
-    /* Optionally set a name for the object */
-    ctr.name("CTR");
+    Serial.begin(9600);
 
-    /* Reverse: On = LOW, Off = HIGH */
-    //ctr.reverse(true);
+    is(temp.name("temp") == F("temp"), "Temp name");
+    is(hum.name("hum") == F("hum"), "Hum name");
+    is(cyc.name("cyc") == F("cyc"), "Cyc name");
 
-    /* Mode: 0 = Cool, 1 = Heat; Default Cool */
-    //ctr.mode(MODE_HEAT);
+    is(temp.reverse(true) == true, "Temp rev true"
+    is(hum.reverse(true) == true, "Hum rev true"
+    is(cyc.reverse(true) == true, "Cyc rev true"
+
+    is(temp.reverse(false) == false, "Temp rev false"
+    is(hum.reverse(false) == false, "Hum rev false"
+    is(cyc.reverse(false) == false, "Cyc rev false"
+
+    is(temp.mode() == MODE_COOL, "Temp mode default");
+    is(temp.mode(MODE_HEAT) == MODE_HEAT, "Temp mode heat");
+    is(temp.mode(MODE_COOL) == MODE_COOL, "Temp mode cool");
+    is(temp.factor() == 1, "Temp factor default");
+    is(temp.factor(3) == 3, "Temp factor 3");
+    is(temp.factor(1) == 1, "Temp factor 1");
+
+    is(hum.mode() == MODE_HUMIDIFY, "Hum mode default");
+    is(hum.mode(MODE_DEHUMIDIFY) == MODE_DEHUMIDIFY, "Hum mode dehum");
+    is(hum.mode(MODE_HUMIDIFY) == MODE_HUMIDIFY, "Hum mode hum");
+    is(hum.factor() == 1, "Hum factor default");
+    is(hum.factor(3) == 3, "Hum factor 3");
+    is(hum.factor(1) == 1, "Hum factor 1");
 }
 
 void loop() {
@@ -59,4 +78,9 @@ void loop() {
 
     /* Disable the relay */
     //ctr.pin(-1);
+}
+
+void is (bool arg, const char* msg) {
+    Serial.print(msg);
+    arg ? Serial.println(F(": OK")) : Serial.println(F(": FAIL"));
 }
